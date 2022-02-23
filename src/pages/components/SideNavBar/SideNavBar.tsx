db3b60c3d045be09af3a2711d7f4ec658a567f27
Thomas Francis <thomasfrancis@Thomass-MacBook-Air.local>
2022-02-23 15:54:48 -0500

    finished the sticky side bar component

--- src/pages/components/SideNavBar/SideNavBar.tsx
+++ src/pages/components/SideNavBar/SideNavBar.tsx
@@ -1,39 +1,46 @@
 import React from "react"
 
+const TopItems: string[] = [
+    'home',
+    'resumé',
+    'symbols',
+]
+const BottomItems: string[] = [
+    'semblance.us',
+    'Holberton Headphones',
+    'Frontend School Projects',
+    'Backend School Projects',
+    'Gates of Shell',
+    'Stock Trading Bot',
+    'Simple Lead Generator',
+    'Broker Checker',
+    'This Website!'
+]
+
+
 export const SideNavBar = () => {
     return (
         <nav className="bg-lightPink sticky left-0 md:w-3/12 w-3/4 h-screen z-10 top-0 content-center">
             <ul className="grid md:gap-4 gap-2 grid-rows place-content-center h-full">
-                <li className="text-right">
-                    <a href="#home">
-                        Home
-                    </a>
-                </li>
-                <li className="text-right">
-                    resumé
-                </li>
-                <li className="text-right">
-                    github symbol, linkedin symbol
-                </li>
+                {TopItems.map((item: string) => {
+                    return <SideNavBarItem item={item} />
+                })}
                 <hr></hr>
-                <li className="text-right">
-                    semblance.us
-                </li>
+                {BottomItems.map((item: string) => {
+                    return <SideNavBarItem item={item} />
+                })}
             </ul>
         </nav>
     )
 }
-/*
-const SideNavBarItem = () => {
-    const items: Array<String> = [
-        'home',
-        'resumé',
-        'symbols',
-        'semblance.us',
-    ]
+
+const SideNavBarItem = ({ item }: { item: string }) => {
+    const ref = '#' + item.toLowerCase().replace(/ /g, '-')
     return (
         <li className="text-right">
-
+            <a href={ref}>
+                {item}
+            </a>
+        </li>
     )
 }
-*/
